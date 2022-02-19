76a8e102c8c36548926583e39119dddd07ddf478
Paulo Nogueira <paulrpn@live.com>
2022-02-19 01:52:08 -0300

    conexao com database

---
+++ src/models/connectionDB.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const MONGO_DB_URL = `mongodb://${process.env.HOST}:27017/profranchcafe`;
+const DB_NAME = 'profranchcafe';
+
+module.exports = () => MongoClient.connect(MONGO_DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then((connection) => connection.db(DB_NAME))
+  .catch((err) => {
+    // eslint-disable-next-line no-console
+    console.error(err);
+    process.exit(1);
+  });
